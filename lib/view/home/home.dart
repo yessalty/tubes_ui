0cfa981cf270f55897937f78c9948f5091d9a0c1
yessalty <yessanugraheni69@gmail.com>
2023-12-02 23:32:26 +0700

    homepage kurang carousel

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -20,14 +20,11 @@ class _HomepageState extends State<Homepage> {
     });
   }
 
-  static List<Widget> _widgetOptions = <Widget>[
+  List<Widget> _widgetOptions = <Widget>[
     HomeView(),
     BookPage(),
     ProfilePage(),
   ];
-
-  String location = 'Yogyakarta';
-  String searchQuery = '';
 
   @override
   Widget build(BuildContext context) {
@@ -63,15 +60,326 @@ class _HomepageState extends State<Homepage> {
 }
 
 class HomeView extends StatefulWidget {
-  const HomeView({super.key});
+  const HomeView({Key? key}) : super(key: key);
 
   @override
   State<HomeView> createState() => _HomeViewState();
 }
 
 class _HomeViewState extends State<HomeView> {
+  String location = 'Yogyakarta';
+  String searchQuery = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(2.0),
+                child: Row(
+                  children: [
+                    const Icon(Icons.location_on),
+                    const SizedBox(width: 8.0),
+                    const Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Your Location',
+                          style: TextStyle(fontSize: 12),
+                        ),
+                        Text(
+                          'Yogyakarta',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProfilePage()),
+                        );
+                      },
+                      child: const CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/gojohh.jpg'),
+                      ),
+                    ),
+                    const SizedBox(width: 10.0),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: TextFormField(
+                        onChanged: (value) {
+                          setState(() {
+                            searchQuery = value;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Search',
+                          contentPadding: const EdgeInsets.symmetric(
+                              horizontal: 16.0, vertical: 14.0),
+                          border: InputBorder.none,
+                          suffixIcon: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.transparent,
+                              elevation: 0,
+                            ),
+                            onPressed: () {
+                              // Search logic using searchQuery
+                            },
+                            child:
+                                const Icon(Icons.search, color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Discover the latest deals and updates',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => PromoPage()),
+                        );
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage('assets/images/promo.jpeg'),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        height: 150,
+                        child: Center(
+                          child: Text(
+                            'PROMO',
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              backgroundColor:
+                                  const Color.fromARGB(255, 74, 73, 73)
+                                      .withOpacity(0.5),
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Popular cars near you',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => OpenCarPage()),
+                        );
+                      },
+                      child: Card(
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        child: Container(
+                          width: 200,
+                          height: 320,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              ClipRRect(
+                                borderRadius: const BorderRadius.vertical(
+                                    top: Radius.circular(4)),
+                                child: Image.asset(
+                                  'assets/images/car1.jpeg',
+                                  width: 200,
+                                  height: 200,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.all(12),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'GR YARIS',
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(height: 8),
+                                    Row(
+                                      children: [
+                                        Icon(Icons.settings_input_component,
+                                            size: 18),
+                                        SizedBox(width: 4),
+                                        Text('A/T'),
+                                        SizedBox(width: 12),
+                                        Icon(Icons.people, size: 18),
+                                        SizedBox(width: 4),
+                                        Text('5'),
+                                        SizedBox(width: 12),
+                                        Icon(Icons.car_rental, size: 18),
+                                        SizedBox(width: 4),
+                                        Text('2'),
+                                        SizedBox(width: 12),
+                                        Icon(Icons.wb_sunny, size: 18),
+                                        SizedBox(width: 4),
+                                        Text('A/C'),
+                                      ],
+                                    ),
+                                    SizedBox(height: 8),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Icon(
+                                              Icons.check,
+                                              size: 18,
+                                              color: Color.fromRGBO(
+                                                  127, 90, 240, 1),
+                                            ),
+                                            SizedBox(width: 4),
+                                            Text('Instant Confirmation'),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text('Yogyakarta'),
+                                            SizedBox(width: 4),
+                                            Icon(
+                                              Icons.location_on,
+                                              size: 18,
+                                              color: Color.fromRGBO(
+                                                  127, 90, 240, 1),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        SizedBox(height: 8),
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Icon(
+                                              Icons.check,
+                                              size: 18,
+                                              color: Color.fromRGBO(
+                                                  127, 90, 240, 1),
+                                            ),
+                                            SizedBox(width: 4),
+                                            Text('Free Cancellation'),
+                                          ],
+                                        ),
+                                        SizedBox(width: 70),
+                                        Row(
+                                          children: [
+                                            Text('Day/'),
+                                            SizedBox(width: 4),
+                                            Text(
+                                              'Rp 500.000',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Color.fromRGBO(
+                                                    127, 90, 240, 1),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Popular City',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => ProfilePage()),
+                        );
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage('assets/images/promo.jpeg'),
+                              fit: BoxFit.cover),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        height: 100,
+                        child: const Center(
+                          child: Text(
+                            'YOGYAKARTA',
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
